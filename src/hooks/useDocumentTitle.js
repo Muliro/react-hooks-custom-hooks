fcdd8daf3e4df8812ea518795bf1d36749527429
michael muliro <michael.muliro@student.moringaschool.com>
2022-07-24 00:21:26 +0300

    Done.

--- src/hooks/useDocumentTitle.js
+++ src/hooks/useDocumentTitle.js
@@ -1,9 +1,13 @@
 import { useEffect } from "react";
 
+
+
+
+
+
 function useDocumentTitle(pageTitle) {
   useEffect(() => {
     document.title = pageTitle;
   }, [pageTitle]);
 }
-
 export default useDocumentTitle;
